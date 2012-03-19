97fa74172dd4b6ab1998b7f90f7da096e00cf299
Dan Sweeney <dan@floatingboxes.com>
2012-03-18 19:45:12 -0500

    added the partners page

--- partners/index.php
+++ partners/index.php
@@ -1,21 +1,115 @@
 <?php $page_id = "partners"; ?>
 <?php include($_SERVER['DOCUMENT_ROOT'].'/inc/header.php');?>
-<h1><?php echo $page_id ?></h1>
 
-<dl>
-	<dt>Section:</dt>
-	<dd><?php echo $seo[$page_id]["section"] ?></dd>
-	
-	<dt>Type:</dt>
-	<dd><?php echo $seo[$page_id]["type"] ?></dd>
-	
-	<dt>Title:</dt>
-	<dd><?php echo $seo[$page_id]["title"] ?></dd>
-	
-	<dt>Keywords:</dt>
-	<dd><?php echo $seo[$page_id]["keywords"] ?></dd>
-	
-	<dt>Description:</dt>
-	<dd><?php echo $seo[$page_id]["description"] ?></dd>
-</dl>
+<div class="row pagehead">
+	<div class="span12">
+		<h1 class="cushycms" title="Heading">Partners</h1>
+	</div>
+</div>
+<div class="row">
+	<div class="span8" title="Content">
+		<div class="partner hr">
+			<div class="row">
+				<div class="span2 logo">
+					<img src="/img/logo-erc.png" alt="ERC Dataplus" class="cushycms">
+				</div>
+				<div class="span6 description cushycms">
+					<h3><a href="http://www.ercdataplus.com" name="erc-datapluss">ERC Dataplus</a></h3>
+					<p>Since 1993, ERC Dataplus has been helping organizations with their most pressing business challenge: finding and keeping qualified talent. With its comprehensive and integrated technology solutions, ERC enables clients to save time, reduce costs and reduce the administrative burdens associated with the recruiting, selection, development and retention of talented employees.</p>
+					<p><a href="http://www.ercdataplus.com">www.ercdataplus.com</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="partner hr">
+			<div class="row">
+				<div class="span2 logo">
+					<img src="/img/logo-skillsurvey.png" alt="SkillSurvey" class="cushycms">
+				</div>
+				<div class="span6 description cushycms">
+					<h3><a href="http://www.skillsurvey.com" name="skillsurvey">SkillSurvey</a></h3>
+					<p>SkillSurvey was formed in 2001 to help organizations around the world make better hiring decisions more efficiently and cost-effectively.</p>
+					<p>The premise behind our enterprise solution is that traditional phone reference-checks are an outmoded and inadequate way for recruiters and hiring managers to seek objective feedback on a job candidate's past performance.  Our clients believe that it is much better to gather a "360-degree" assessment of a candidate's behaviors, skills and developmental needs, by using a survey that operates automatically and all through email.</p>
+					<p><a href="http://www.skillsurvey.com">www.skillsurvey.com</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="partner hr">
+			<div class="row">
+				<div class="span2 logo">
+					<img src="/img/logo-honkamp.png" alt="Honkamp Krueger &amp; Co., P.C." class="cushycms">
+				</div>
+				<div class="span6 description cushycms">
+					<h3><a href="http://www.honkamp.com" name="honkamp-krueger-and-co">Honkamp Krueger &amp; Co., P.C.</a></h3>
+					<p>For over 60 years, Honkamp Krueger &amp; Co., P.C. has been helping businesses ranging from small family-owned businesses to Fortune 500 companies reach their goals.   With our vast experience, we will recognize the unique challenges and circumstances of your business. Also, our deep menu of services allows us to meet your specific needs. We will listen to you, build a healthy, productive business relationship with you, and help your business reach the next level.</p>
+					<p><a href="http://www.honkamp.com">www.honkamp.com</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="partner hr">
+			<div class="row">
+				<div class="span2 logo">
+					<img src="/img/logo-tazworks.png" alt="" class="cushycms">
+				</div>
+				<div class="span6 description cushycms">
+					<h3><a href="http://www.tazworks.com" name="tazworks">TazWorks</a></h3>
+					<p>InstaScreen Premiere 2.0™ meets and exceeds the diverse background screening requirements of the business, property management, volunteer organization, student/intern, and healthcare industries.  Our Simple. Powerful.™ technology ensures that no matter the market segment, your clients receive accurate data quickly, securely, and in an easy-to-understand format.</p>
+					<p><a href="http://www.tazworks.com">www.tazworks.com</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="partner hr">
+			<div class="row">
+				<div class="span2 logo">
+					<img src="/img/logo-anchor.png" alt="Anchor Planning Group" class="cushycms">
+				</div>
+				<div class="span6 description cushycms">
+					<h3><a href="http://www.anchorplanninggroup.com" name="anchor-planning-group">Anchor Planning Group</a></h3>
+					<p>Anchor was created on a vision to build a new firm that focuses on innovation in the executive search, benefits, insurance, training and HR consulting marketplace. As a company and as individuals, we value integrity, honesty, openness, personal excellence, continual self-improvement and mutual respect. All aspects of our business are based on the highest standards of both client and individual care.</p>
+					<p><a href="http://www.anchorplanninggroup.com">www.anchorplanninggroup.com</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="partner hr">
+			<div class="row">
+				<div class="span2 logo">
+					<img src="/img/logo-jobtarget.png" alt="JobTarget" class="cushycms">
+				</div>
+				<div class="span6 description cushycms">
+					<h3><a href="http://www.jobtarget.com" name="job-target">JobTarget</a></h3>
+					<p>JobTarget is the largest third-party operator of niche job boards in the world and the first company to offer an integrated cross-posting/job distribution and strategy optimization technology solution. We have been rapidly expanding in the past few years and have won numerous awards, including being named 871 on the Inc. 5000 list of America’s Fastest Growing companies. Our suite of Products &amp; Services provide solutions for job seekers, employers, media / website publishers, and many other organizations on a daily basis.</p>
+					<p><a href="http://www.jobtarget.com">www.jobtarget.com</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="partner hr">
+			<div class="row">
+				<div class="span2 logo">
+					<img src="/img/logo-hrtesting.png" alt="HR Testing" class="cushycms">
+				</div>
+				<div class="span6 description cushycms">
+					<h3><a href="http://www.hrtesting.com" name="hr-testing">HR Testing</a></h3>
+					<p>HRT’s mission is simple, to help organizations identify and select the best person for the job…the first time… through the use of professionally developed, valid, reliable and legally defensible pre-employment testing tools.  Backed by validity research and founded on industrial/organizational psychology principles, HRT assessments offer a cost effective solution for increased job fit, increased productivity and reduced turnover.</p>
+					<p><a href="http://www.hrtesting.com">www.hrtesting.com</a></p>
+				</div>
+			</div>
+		</div>
+		<div class="partner">
+			<div class="row">
+				<div class="span2 logo">
+					<img src="/img/logo-escreen.png" alt="eScreen" class="cushycms">
+				</div>
+				<div class="span6 description cushycms">
+					<h3><a href="http://www.escreen.com" name="escreen">eScreen</a></h3>
+					<p>eScreen, Inc. is a technology-enabled Third-Party Administrator that provides next-generation employment screening applications for hiring and maintaining efficient workforces. eScreen reduces program costs, incorporates a paperless process, and automates compliance management.</p>
+					<p><a href="http://www.escreen.com">www.escreen.com</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="span4">
+		<ul class="nav nav-tabs nav-stacked">
+			<?php include($_SERVER['DOCUMENT_ROOT'].'/inc/nav-partners.php');?>
+		</ul>
+	</div>
+</div>
 <?php include($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php');?>
