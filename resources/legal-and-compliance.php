65f52f7fc93b99d34c7172482d0090e240f2091d
Dan Sweeney <dan@floatingboxes.com>
2012-11-06 21:15:29 -0600

    updated legal and compliance page

--- resources/legal-and-compliance.php
+++ resources/legal-and-compliance.php
@@ -14,7 +14,7 @@
 			<li><a href="http://www.accessreports.com/statutes/DPPA1.htm">Drivers Privacy Protection Act</a></li>
 			<li><a href="http://www.ftc.gov/os/statutes/031224fcra.pdf">Fair Credit Reporting Act (FCRA)</a></li>
 			<li><a href="http://www.treasury.gov/offices/domestic-finance/financial-institution/cip/pdf/fact-act.pdf">Fair and Accurate Credit Transaction Act (FACTA)</a></li>
-			<li><a href="http://www.cdiaonline.org/pdf/GLB_Teleseminar_FTC_Final_Disposal_Rule.pdf">FTC Disposal Rule</a></li>
+			<li><a href="http://www.ftc.gov/opa/2005/06/disposal.shtm">FTC Disposal Rule</a></li>
 			<li><a href="http://banking.senate.gov/conf/grmleach.htm">Gramm-Leach-Bliley Act (GLB)</a></li>
 			<li><a href="http://www.napbs.com">National Association of Professional Background Screeners (NAPBS)</a></li>
 			<li><a href="http://www.eeoc.gov/">Equal Employment Opportunity Commission</a></li>
@@ -23,6 +23,14 @@
 			<li><a href="http://www.eeoc.gov/policy/adea.html">The Age Discrimination in Employment Act of 1967</a></li>
 			<li><a href="http://www.eeoc.gov/policy/cra91.html">The Civil Rights Act of 1991</a></li>
 			<li><a href="http://www.eeoc.gov/laws/guidance/arrest_conviction.cfm">EEOC Enforcement Guidance on the Consideration of Arrest and Conviction Records in Employment Decisions Under Title VII of the Civil Rights Act of 1964</a></li>
+		</ul>
+		
+		<h2>Other FCRA Related Forms</h2>
+		<ul>
+			<li><a href="/files/fcra/protocol-for-adverse-action-2012.docx">Adverse Action Protocol</a></li>
+			<li><a href="/files/fcra/notice-to-users-of-consumer-reports-2013.docx">Notice To Users of Consumer Reports</a></li>
+			<li><a href="/files/fcra/remedying-the-effects-of-identity-theft-2013.docx">Remedying the Effects of Identity Theft</a></li>
+			<li><a href="/files/fcra/notice-to-furnishers-2013.docx">Notice to Furnishers of Information</a></li>
 		</ul>
 	</div>
 	<div class="span4">
