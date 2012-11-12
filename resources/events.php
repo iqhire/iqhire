b405b5944e007b84a241a427a9d6d26b32386d46
Dan Sweeney <dan@floatingboxes.com>
2012-11-11 19:42:56 -0600

    updated events page

--- resources/events.php
+++ resources/events.php
@@ -7,22 +7,12 @@
 </div>
 <div class="row">
 	<div class="span8 cushycms" title="Content">
-		<h4>
-			August 30, 2012</h4>
+		<h4>January 2, 2013</h4>
 		<p>
-			Event: Juneau County UW-Extension Meeting<br />
-			Sponsor: UW Extension, Juneau County Community Development<br />
-			Location: Mauston, WI<br />
-			Topic: <em>Top 10 Background Check Mistakes</em><br />
-			Speaker: Alan Kinsey<br />
-			&nbsp;</p>
-		<h4>
-			October 9, 2012</h4>
-		<p>
-			Event: Decatur SHRM Conference<br />
-			Sponsor: Decatur, IL SHRM Chapter<br />
-			Location: Decatur, IL<br />
-			Topic: <em>Top 10 Background Check Mistakes</em><br />
+			Event: Mississippi Valley HR Association Chapter Meeting<br/>
+			Sponsor: Mississippi Valley HR Association<br/>
+			Location: Burlington, Iowa<br/>
+			Topic: Advanced Screening Techniques<br/>
 			Speaker: Alan Kinsey</p>
 	</div>
 	<div class="span4">
