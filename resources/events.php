c5a7b90418dedff3f28bd6f05670c57f699bb50a
Dan Sweeney <dan@floatingboxes.com>
2013-11-12 08:46:57 -0600

    updated events

--- resources/events.php
+++ resources/events.php
@@ -7,94 +7,6 @@
 </div>
 <div class="row">
 	<div class="span8 cushycms">
-		<h4>January 2, 2013</h4>
-		<p>
-			Event: Mississippi Valley HR Association Chapter Meeting<br/>
-			Sponsor: Mississippi Valley HR Association<br/>
-			Location: Burlington, Iowa<br/>
-			Topic: Advanced Screening Techniques<br/>
-			Speaker: Alan Kinsey</p>
-
-		<h4>January 8, 2013</h4>
-		<p>
-			Event: Quincy-Hannibal SHRM Chapter Meeting<br/>
-			Sponsor: Quincy-Hannibal SHRM Chapter<br/>
-			Location: Quincy, Illinois<br/>
-			Topic: Top 10 Background Check Mistakes<br/>
-			Speaker: Alan Kinsey</p>
-
-
-		<h4>February 12, 2013</h4>
-		<p>
-			Event: Central Iowa SHRM Chapter Meeting<br/>
-			Sponsor:Central Iowa SHRM<br/>
-			Location: West Des Moines, Iowa<br/>
-			Topic: Employment Branding<br/>
-			Speaker: Alan Kinsey</p>
-
-		<h4>February 12, 2013</h4>
-		<p>
-			Event: EIHRA Chapter Meeting<br/>
-			Sponsor: Eastern Iowa Human Resources Association<br/>
-			Location: Cedar Rapids, Iowa<br/>
-			Topic: Top 10 Background Check Mistakes<br/>
-			Speaker: Jim Sweeney</p>
-			
-		<h4>February 19, 2013</h4>
-		<p>
-			Event: HRMA of Northwest Missouri Chapter Meeting<br/>
-			Sponsor: HRMA of Northwest Missouri<br/>
-			Location: St. Joseph, Missouri<br/>
-			Topic: Top 10 Background Check Mistakes<br/>
-			Speaker: Alan Kinsey</p>
-
-		<h4>March 20, 2013</h4>
-		<p>
-			Event: Illinois Valley SHRM Chapter Meeting<br/>
-			Sponsor: Illinois Valley SHRM<br/>
-			Location: Peru, Illinois<br/>
-			Topic: Advanced Screening Techniques<br/>
-			Speaker: Alan Kinsey</p>
-
-			
-		<h4>March 26, 2013</h4>
-		<p>
-			Event: Bloomington-Normal HR Council Meeting<br/>
-			Sponsor: Bloomington-Normal Human Resource Council<br/>
-			Location: Illinois State University - Normal, Illinois<br/>
-			Topic: Advanced Screening Techniques<br/>
-			Speaker: Alan Kinsey</p>
-
-		<h4>April 18, 2013</h4>
-		<p>
-			Event: Iowa Contractors Conference<br/>
-			Location: Ames, Iowa<br/>
-			Topic: Fraud Prevention<br/>
-			Speaker: Jim Sweeney</p>
-			
-		<h4>April 25, 2013</h4>
-		<p>
-			Event: IACUBO/Iowa CUPA-HR Joint Annual Conference<br/>
-			Location: Ames, Iowa<br/>
-			Topic: Advanced Screening Techniques<br/>
-			Speaker: Alan Kinsey</p>
-
-		<h4>October 4, 2013</h4>
-		<p>
-			Event: Iowa State SHRM Conference<br/>
-			Sponsor: Iowa State SHRM Council<br/>
-			Location: Coralville Marriott – Coralville, Iowa<br/>
-			Topic: Top 10 Background Check Mistakes<br/>
-			Speaker: Alan Kinsey</p>
-
-
-		<h4>October 4, 2013</h4>
-		<p>
-			Event: Iowa State SHRM Conference<br/>
-			Sponsor: Iowa State SHRM Council<br/>
-			Location: Coralville Marriott – Coralville, Iowa<br/>
-			Topic: Advanced Screening Techniques<br/>
-			Speaker: Alan Kinsey</p>
 		
 		<h4>November 20, 2013</h4>
 		<p>Event: Tri-State SHRM Chapter Meeting<br/>
@@ -111,14 +23,71 @@
 			Topic: Advanced Screening Techniques<br/>
 			Speaker: Alan Kinsey</p>
 
-		 
-
 		<h4>December 13, 2013</h4>
 		<p>Event: Verisight HR Roundtable<br/>
 			Sponsor: Verisight,Inc.<br/>
 			Location: Cedar Rapids Country Club – Cedar Rapids, Iowa<br/>
 			Topic: Advanced Screening Techniques<br/>
 			Speaker: Alan Kinsey</p>
+
+		<h4>January 8, 2014</h4>
+		<p>Event: Mississippi Valley HR Association Chapter Meeting<br/>
+			Sponsor: Mississippi Valley HR Association<br/>
+			Location: Burlington, Iowa<br/>
+			Topic: How HR Can Have a Larger Role in Setting Strategy<br/>
+			Speaker: Alan Kinsey
+
+		<h4>January 14, 2014</h4>
+		Event: Quincy Area SHRM Chapter Meeting<br/>
+			Sponsor: Quincy Area Chapter of SHRM<br/>
+			Location: Quincy, IL<br/>
+			Topic: How HR Can Have a Larger Role in Setting Strategy<br/>
+			Speaker: Alan Kinsey
+
+			 
+		<h4>January 28, 2014</h4>
+		<p>Event: Rock River Human Resources Association Chapter Meeting<br/>
+			Sponsor: Rock River HRA<br/>
+			Location: Dixon, IL<br/>
+			Topic: To Be Determined<br/>
+			Speaker: Alan Kinsey<p>
+
+			 
+		<h4>February 5, 2014</h4>
+		<p>Event: Alabama Bankers Association HR Conference<br/>
+			Sponsor: Alabama Bankers Association<br/>
+			Location: Embassy Suites – Hoover, AL<br/>
+			Topic: Top 10 Background Check Mistakes<br/>
+			Speaker: Alan Kinsey</p>
+
+			 
+
+		<h4>March 18, 2014</h4>
+		<p>Event: Great River HR Association Chapter Meeting<br/>
+			Sponsor: Great River HRA<br/>
+			Location: Davenport, Iowa<br/>
+			Topic: Advanced Screening Techniques<br/>
+			Speaker: Alan Kinsey</p>
+
+			 
+
+		<h4>March 19, 2014</h4>
+		<p>Event: Central Illinois SHRM Chapter Meeting<br/>
+			Sponsor: Central Illinois SHRM Chapter<br/>
+			Location: Springfield, IL<br/>
+			Topic: Advanced Screening Techniques<br/>
+			Speaker: Alan Kinsey</p>
+
+			 
+
+		<h4>May 9, 2014</h4>
+		<p>Event: West Central Wisconsin SHRM Chapter Meeting<br/>
+			Sponsor: West Central Wisconsin SHRM Chapter<br/>
+			Location: Tomah, WI<br/>
+			Topic: Advanced Screening Techniques<br/>
+			Speaker: Alan Kinsey</p>
+
+
 	</div>
 	<div class="span4">
 		<ul class="nav">
