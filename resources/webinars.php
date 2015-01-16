e1566b91457f5c3035a9add4c897d53d89930cd8
Dan Sweeney <dan@floatingboxes.com>
2015-01-15 20:06:45 -0600

    updates to webinar page

--- resources/webinars.php
+++ resources/webinars.php
@@ -10,40 +10,24 @@
 		<p class="intro">Inquirehire offers free, educational webinars designed to help employers stay abreast of the latest trends and best practices being utilized in today’s hiring process. Each webinar is structured to last no more than 30 minutes so employers can work them into their busy schedules.</p>
 		<p>If you would like a custom webinar for your staff or organization, please contact solutionsupport@inquirehire.com or click the button below.</p>
 
+		<h2>Sample Webinars</h2>
+
 		<h3>Layers of Protection</h3>
 		<p>Criminal records are stored in a variety of jurisdictions and repositories, and no single search can provide a full and comprehensive view of an applicant's prior criminal record. This webinar reviews the essential components of a criminal record check including where criminal records are stored, the key differences in sources of information and how to design a criminal record check protocol that properly protects your organization.</p>
-		<p><strong>Next Webinar</strong><br/>
-			Friday, January 25, 2013<br/>
-			1:30 pm central<br/>
-			Duration: 45 minutes</p>
-		<form action="webinar-signup" method="post" accept-charset="utf-8">
-			<input type="hidden" name="webinar" value="Layers of Protection" id="webinar">
-			<p><input type="submit" value="Request a Webinar" class="button"></p>
-		</form>
-
 
 		<h3>Advanced Employment Screening – Social Media</h3>
 		<p>Why should I consider social media screening as part of our pre-employment screening protocol? Social media screening can reveal applicant behavior that may be embarrassing or damaging to your organization. Many organizations use internal resources to conduct some form of social media screening, but this can be time-consuming, yet incomplete and inconsistent. In this webinar we will discuss a more effective approach to social media screening that is also comprehensive and compliant.</p>
-		<p><strong>Next Webinar</strong><br/>
-			Friday, January 11, 2013<br/>
-			1:30 pm central<br/>
-			Duration:  30 minutes</p>
-		<form action="webinar-signup" method="post" accept-charset="utf-8">
-			<input type="hidden" name="webinar" value="Advanced Employment Screening – Social Media" id="webinar">
-			<p><input type="submit" value="Request a Webinar" class="button"></p>
-		</form>
 
 
 		<h3>Advanced Employment Screening – Professional Reference Checks</h3>
 		<p>Historically 40% of candidates falsify their work history by embellishing responsibilities or accomplishments. Most employers will not provide non-neutral reference information to prospective employers. As a result, a critical “blind spot” can exist for many job applicants. In this webinar we will discuss a solution that will produce 3 or more professional references in 1 or 2 business days without time-consuming, fruitless phone calls.</p>
-		<p><strong>Next Webinar</strong><br/>
-			Friday, January 18, 2013<br/>
-			1:30 pm central<br/>
-			Duration: 30 minutes</p>
+
+
 		<form action="webinar-signup" method="post" accept-charset="utf-8">
-			<input type="hidden" name="webinar" value="Advanced Employment Screening – Professional Reference Checks" id="webinar">
+			<input type="hidden" name="webinar" value="Webinar Signup" id="webinar">
 			<p><input type="submit" value="Request a Webinar" class="button"></p>
 		</form>
+
 
 		
 	</div>
