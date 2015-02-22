2ac337423fd6ea21c8a633b83825d89ca4535b3c
Dan Sweeney <dan@floatingboxes.com>
2015-02-21 19:30:40 -0600

    wip: menu

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
-  $(".nav__item").click(function(){
-    $(this).toggleClass("is-open");
+  $(".nav-section").click(function(){
+    $(this).find(".nav-sub").toggleClass("is-open");
     return false;
+  });
+
+  $(".nav-menu").click(function(){
+    $(this).toggleClass("is-open");
+    $(".nav-sections").toggleClass("is-open");
   });
 });
