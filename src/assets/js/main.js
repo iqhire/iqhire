3891a61cbdc5acd8b46d9b8bc1910bb37ef4a114
Dan Sweeney <dan@floatingboxes.com>
2016-04-16 16:21:47 -0500

    hero javascript

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+  //- Navigation
   $(".js-nav-section > a").click(function(){
     var isOpen = $(this).next().hasClass("is-open");
 
@@ -14,4 +16,36 @@ $(document).ready(function(){
     $(this).toggleClass("is-open");
     $(".js-nav-sections").toggleClass("is-open");
   });
+
+  //- Home Page Hero
+  $(".js-hero-menu-item").click(function(){
+    $(".js-hero-menu-item").removeClass("is-selected");
+    $(this).addClass("is-selected");
+
+    if ($(this).hasClass("js-hero-menu-item--attract")) {
+      changeHeroBackground("attract");
+      changeHeroContent("attract");
+    } else if ($(this).hasClass("js-hero-menu-item--avoid")) {
+      changeHeroBackground("avoid");
+      changeHeroContent("avoid");
+    } else if ($(this).hasClass("js-hero-menu-item--streamline")) {
+      changeHeroBackground("streamline");
+      changeHeroContent("streamline");
+    } else {
+      console.log("error :(");
+    }
+
+    return false;
+  });
+
 });
+
+function changeHeroBackground(hero) {
+  $(".js-hero-background").removeClass("is-showing--attract is-showing--avoid is-showing--streamline");
+  $(".js-hero-background").addClass("is-showing--"+hero);
+}
+
+function changeHeroContent(hero) {
+  $(".js-hero").removeClass("is-showing");
+  $(".js-hero--"+hero).addClass("is-showing");
+}
